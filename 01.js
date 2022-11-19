52f4e4906c0eb1ebe3f5367197e747599bd92de7
lifuxuan <1647021316@qq.com>
2022-11-19 19:09:30 +0800

    测试1

---
+++ 01.js
@@ -0,0 +1,9 @@
+/*
+ * @Author: lifuxuan 1647021316@qq.com
+ * @Date: 2022-11-19 18:24:14
+ * @LastEditors: lifuxuan 1647021316@qq.com
+ * @LastEditTime: 2022-11-19 18:24:21
+ * @FilePath: \Git\01.js
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+console.log(66);
